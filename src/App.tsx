503846b18d40a0b574c0e945742c7131a6e98397
exotiicx402 <support@turnstilefndn.org>
2025-10-31 21:28:35 -0700

    Remove leaderboard and wallet provider to fix build

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { WalletContextProvider } from './components/WalletProvider'
 import Home from './pages/Home'
 import Docs from './pages/Docs'
-import Leaderboard from './pages/Leaderboard'
 import Marketplace from './pages/Marketplace'
 
 function App() {
   return (
-    <WalletContextProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/docs" element={<Docs />} />
-          <Route path="/leaderboard" element={<Leaderboard />} />
-          <Route path="/marketplace" element={<Marketplace />} />
-          {/* Hidden platform route for development */}
-          <Route path="/v1" element={<Marketplace />} />
-        </Routes>
-      </Router>
-    </WalletContextProvider>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/docs" element={<Docs />} />
+        {/* Hidden platform route for development */}
+        <Route path="/v1" element={<Marketplace />} />
+      </Routes>
+    </Router>
   )
 }
 
